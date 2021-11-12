157a5b8bbd6c0ba17ffe16097b4cf60140d4bcec
aashabul <aashabulimam@gmail.com>
2021-11-13 02:05:24 +0600

    form added

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -1,8 +1,8 @@
-import { Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
+import { Button, Card, CardContent, CardMedia, Grid, TextField, Typography } from '@mui/material';
 
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-// import useAuth from '../../Hooks/useAuth';
+import useAuth from '../../Hooks/useAuth';
 import Footer from '../Shared/Footer/Footer';
 import Navigation from '../Shared/Navigation/Navigation';
 
@@ -10,8 +10,9 @@ const Purchase = () => {
     const { id } = useParams();
     const [details, setDetails] = useState([]);
     const [itemDetails, setItemDetails] = useState({});
+    const [loginData, setLoginData] = useState({});
 
-    // const { user } = useAuth();
+    const { user } = useAuth();
 
 
     useEffect(() => {
@@ -28,33 +29,90 @@ const Purchase = () => {
         }
     }, [details, id])
 
+    const handleOrderSubmit = e => {
+        alert('order placed')
+        e.preventDefault();
+    }
+
+    const handleOnBlur = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newLoginData = { ...loginData };
+        newLoginData[field] = value;
+        setLoginData(newLoginData);
+        // console.log(field, value);
+        // console.log(newLoginData);
+    }
 
     return (
         <>
             <Navigation></Navigation>
-            <Grid item xs={4} sm={4} md={4}>
-                <Card sx={{ minWidth: 200, boxShadow: 3 }}>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image={itemDetails.img}
-                        style={{ width: 'auto', height: '190px', margin: '0 auto' }}
-                    />
-                    <CardContent>
-                        <Typography sx={{ color: '#8C6897', fontWeight: 700 }} variant="h7" component="div">
-                            {itemDetails.name}
-                        </Typography>
-                        <Typography variant="h6" component="div">
-                            price: {itemDetails.price}$
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            product ID: {id}
-                        </Typography>
-                        {/* <Typography variant="body2" color="text.secondary">
+            <Grid container>
+                <Grid item xs={12} sm={12} md={4}>
+                    <Card sx={{ minWidth: 200, boxShadow: 3 }}>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image={itemDetails.img}
+                            style={{ width: 'auto', height: '190px', margin: '0 auto' }}
+                        />
+                        <CardContent>
+                            <Typography sx={{ color: '#8C6897', fontWeight: 700 }} variant="h7" component="div">
+                                {itemDetails.name}
+                            </Typography>
+                            <Typography variant="h6" component="div">
+                                price: {itemDetails.price}$
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                product ID: {id}
+                            </Typography>
+                            {/* <Typography variant="body2" color="text.secondary">
                             {itemDetails.description.color}
                         </Typography> */}
-                    </CardContent>
-                </Card>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} sm={12} md={8}>
+                    <Typography variant="h6" gutterBottom component="div" >
+                        Place Order
+                    </Typography>
+
+                    <form onSubmit={handleOrderSubmit}>
+                        <TextField sx={{ width: '75%', m: 1 }}
+                            // onBlur={handleOnBlur}
+                            name="name"
+                            id="standard-basic1" label={"Your Name"} variant="standard" />
+                        <TextField sx={{ width: '75%', m: 1 }}
+                            // onBlur={handleOnBlur}
+                            name="email"
+                            id="standard-basic" label="Your Email" variant="standard" />
+                        <TextField
+                            sx={{ width: '75%', m: 1 }}
+                            // onBlur={handleOnBlur}
+                            name="password"
+                            id="standard-password-input"
+                            label="Password"
+                            type="password"
+                            autoComplete="current-password"
+                            variant="standard"
+                        />
+                        <TextField
+                            sx={{ width: '75%', m: 1 }}
+                            // onBlur={handleOnBlur}
+                            name="password2"
+                            id="standard-password2-input"
+                            label="Confirm Password"
+                            type="password"
+                            autoComplete="current-password"
+                            variant="standard"
+                        />
+                        <Button
+                            type="submit"
+                            sx={{ width: '30%', m: 1, backgroundColor: '#8C6897' }}
+                            variant="contained">Place Order
+                        </Button>
+                    </form>
+                </Grid>
             </Grid>
             <Footer></Footer>
         </>
